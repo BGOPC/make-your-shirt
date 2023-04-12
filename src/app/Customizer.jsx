eb6194b664bb63118b0a8a3ca352cef089b952f5
BGOPC <bb8933400@gmail.com>
2023-04-13 00:56:52 +0330

    Customizer Template

--- src/app/Customizer.jsx
+++ src/app/Customizer.jsx
@@ -7,8 +7,63 @@ import { download } from "../assets/index.js";
 import { downloadCanvasToImage, reader } from "../config/helpers.js";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants.js";
 import { fadeAnimation, slideAnimation } from "../config/motion.js";
+import {
+  CustomizableButton,
+  Tab,
+  AiPicker,
+  ColorPicker,
+  FilePicker,
+} from "../components";
+
 const Customizer = () => {
-  return <div>Customizer</div>;
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation('left')}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => <Tab
+                  key={tab.name}
+                  tab={tab}
+                  handleClick={() => {
+                  }}
+                />)}
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            className="absolute z-10 top-5 right-5" {...fadeAnimation}
+          >
+            <CustomizableButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => state.intro = true}
+              styles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation('up')}
+          >
+            {FilterTabs.map((tab) => <Tab
+              key={tab.name}
+              tab={tab}
+              isFilterTab
+              isActiveTab
+              handleClick={() => {
+              }}
+            />)}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customizer;
