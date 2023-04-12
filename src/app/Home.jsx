7e6ae2cbf8e0a35566e2894a27784aae9d56bacf
BGOPC <bb8933400@gmail.com>
2023-04-12 23:27:38 +0330

    Basic Home

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from "valtio";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from '../config/motion.js';
+import state from '../store';
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.header {...slideAnimation('down')}>
+            <img src="./threejs.png" alt="threejs logo"
+              className="w-8 h-8 object-contain"/>
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                LET'S <br className='xl:block hidden'/> DO IT
+
+              </h1>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
   );
 };
 
