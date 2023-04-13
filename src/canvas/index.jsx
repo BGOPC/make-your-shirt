7acec01271ff5b7e29f9fbe899d6edc311d8c793
BGOPC <bb8933400@gmail.com>
2023-04-13 14:45:54 +0330

    Size Of shirt

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -8,11 +8,14 @@ import CameraRig from "./CameraRig.jsx";
 
 const CanvasModel = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <Environment preset='city'/>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+    >
+      <ambientLight intensity={0.5}/>
+      <Environment preset="city"/>
       <CameraRig>
-      {/*   <Backdrop/> */}
+        <Backdrop/>
         <Center>
           <Shirt/>
         </Center>
