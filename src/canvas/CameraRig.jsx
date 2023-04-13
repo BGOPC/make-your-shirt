3f94f1e0449e8d262cb6067ab0b0c13fdd03d101
BGOPC <bb8933400@gmail.com>
2023-04-13 14:33:53 +0330

    Camera Rig

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -7,11 +7,39 @@ import state from '../store';
 
 const CameraRig = ({ children }) => {
   const snap = useSnapshot(state);
-  return (
-    <group>
-      {children}
-    </group>
-  );
+  const group = useRef();
+
+  useFrame((objectState, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // set the initial position of the model
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    }
+    else if (isMobile) {
+      targetPosition = [0, 0, 2.5];
+    }
+    else {
+      targetPosition = [0, 0, 2];
+    }
+
+    // set model camera position
+    easing.damp3(objectState.camera.position, targetPosition, 0.25, delta);
+
+    // set the model rotation smoothly
+    easing.dampE(
+        group.current.rotation,
+        [objectState.pointer.y / 10, -objectState.pointer.x / 5, 0],
+        0.25,
+        delta,
+    );
+  });
+
+
+  return <group ref={group}>{children}</group>;
 };
 
 export default CameraRig;
